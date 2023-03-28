ad22c70e57d8c1d161165c7c58a13d0d8e8c45f5
arturkalimullin <noti1996@gmail.com>
2023-03-28 09:27:02 +0300

    Change utils.py

--- utils.py
+++ utils.py
@@ -1,11 +1,11 @@
 import asyncio
 import re
-from datetime import date
+from datetime import date, timedelta
 
 import aiohttp
 import pandas as pd
-from bs4 import BeautifulSoup
 from babel.dates import format_date
+from bs4 import BeautifulSoup
 
 URL_TEMPLATE = 'https://hh.ru/applicant/negotiations?filter=all&page={}'
 
@@ -26,7 +26,14 @@ async def get_responses(page, headers):
             response = []
             for tag in row.find_all(['span', 'button'], {'data-qa': re.compile('^negotiations-item')}):
                 text = tag.text.strip().replace('\xa0', ' ')
-                format_text = text.replace('сегодня', format_date(date.today(), format='long', locale='ru'))
+                dates = ['сегодня', 'вчера', 'позавчера']
+                format_text = text.replace(dates[0], format_date(date.today(), format='long', locale='ru'))
+                format_text = format_text.replace(dates[1],
+                                                  format_date(date.today()-timedelta(days=1), format='long', locale='ru')
+                                                  )
+                format_text = format_text.replace(dates[2],
+                                                  format_date(date.today()-timedelta(days=2), format='long', locale='ru')
+                                                  )
                 response.append(format_text)
             if response:
                 responses.append(response)
@@ -35,7 +42,7 @@ async def get_responses(page, headers):
 
 def create_excel_file(values):
     df = pd.DataFrame(values, columns=['Cтатус', 'Вакансия', 'Компания', 'Дата отклика'])
-    return df.to_csv(index=False)
+    return df.to_csv(index=False, encoding='utf8')
 
 
 async def main(headers, number_pages):
